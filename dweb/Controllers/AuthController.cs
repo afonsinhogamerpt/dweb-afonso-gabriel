347c3d80e51199af48e34566472763514c000687
Afonso Costa <121893968+afonsinhogamerpt@users.noreply.github.com>
2025-04-26 17:39:21 +0100

    Criação da view "Login"
    Apenas o layout do Login foi feito.
    Foi também adicionado tailwindcss ao projeto

---
+++ dweb/Controllers/AuthController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace dweb.Controllers;
+
+public class AuthController : Controller
+{
+    // GET
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Login()
+    {
+        return View();
+    }
+}
