cfad1e538ebaf4d6dd1883dab0781dc730b02e52
GCravo04 <gabicravo04@gmail.com>
2025-07-03 14:34:24 +0100

    Criação de página e métodos para users guardarem filmes
    Criação da página para users guardarem filmes, criação do utilizadorDTO e update dos controllers

--- dweb/Controllers/UserController.cs
+++ dweb/Controllers/UserController.cs
@@ -1,6 +1,8 @@
 ﻿using dweb.Models;
+using dweb.Data;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace dweb.Controllers;
 
@@ -8,10 +10,12 @@ public class UserController : Controller
 {
     
     private readonly UserManager<Utilizador> _userManager;
+    private readonly AppDbContext _context;
 
-    public UserController(UserManager<Utilizador> userManager)
+    public UserController(UserManager<Utilizador> userManager, AppDbContext context)
     {
         _userManager = userManager;
+        _context = context;
     }
     // GET
     public IActionResult Index()
@@ -30,5 +34,43 @@ public class UserController : Controller
         }
 
         return View(utilizador); 
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> GuardarFilme(int filmeId)
+    {
+        var user = await _userManager.GetUserAsync(User);
+        if (user == null)
+        {
+            return Unauthorized();
+        }
+        var filme = await _context.Filme.FindAsync(filmeId);
+        if (filme == null)
+        {
+            return NotFound();
+        }
+        if (user.Filmes == null)
+            user.Filmes = new List<Filme>();
+        if (!user.Filmes.Any(f => f.filmeID == filmeId))
+        {
+            user.Filmes.Add(filme);
+            await _context.SaveChangesAsync();
+        }
+        return RedirectToAction("FilmeDetails", "Filme", new { id = filmeId });
+    }
+    
+    [HttpGet]
+    public async Task<IActionResult> UserFilms()
+    {
+        var user = await _userManager.GetUserAsync(User);
+        if (user == null)
+            return Unauthorized();
+
+        var utilizadorComFilmes = _context.Utilizador
+            .Include(u => u.Filmes)
+            .FirstOrDefault(u => u.Id == user.Id);
+
+        var filmes = utilizadorComFilmes?.Filmes?.ToList() ?? new List<Filme>();
+        return View(filmes);
     }
 }
