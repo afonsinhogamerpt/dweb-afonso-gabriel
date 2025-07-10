b0419cb8592c06ad00b4f7f45c740e3e0622221f
GCravo04 <gabicravo04@gmail.com>
2025-07-10 19:31:53 +0100

    Página de Admin para Cruds para Directors

--- dweb/Controllers/DirectorController.cs
+++ dweb/Controllers/DirectorController.cs
@@ -17,24 +17,56 @@ public class DirectorController : BaseController
     }
     
     [HttpGet]
-    public async Task<ActionResult<IEnumerable<Director>>> GetDirectors()
+    [Route("/Director/Directors")]
+    public IActionResult Directors()
     {
-        var directors =  _context.Director.ToList();
+        var directors = _context.Director.ToList();
+        return View("directors", directors);
+    }
+
+    [HttpPost("update-director")]
+    public async Task<IActionResult> UpdateDirector([FromForm] int directorID, [FromForm] string nome, [FromForm] int idade, [FromForm] string bio)
+    {
+        var d = _context.Director.FirstOrDefault(x => x.directorID == directorID);
+        if (d == null)
+        {
+            return NotFound();
+        }
+        d.nome = nome;
+        d.idade = idade;
+        d.bio = bio;
+        await _context.SaveChangesAsync();
+        return RedirectToAction("DirectorDetails", new { id = directorID });
+    }
+
+    [HttpPost("delete-director")]
+    public async Task<IActionResult> DeleteDirector([FromForm] int directorID)
+    {
+        var director = _context.Director.FirstOrDefault(a => a.directorID == directorID);
+        if (director == null)
+        {
+            return NotFound();
+        }
+        _context.Director.Remove(director);
+        await _context.SaveChangesAsync();
+        return RedirectToAction("Directors");
+    }
+
+    [HttpGet("all")]
+    public ActionResult<IEnumerable<Director>> GetDirectors()
+    {
+        var directors = _context.Director.ToList();
         return directors;
     }
 
     [HttpGet("{id}")]
-    public async Task<IActionResult> GetDirectors(int id)
+    public ActionResult<Director> GetDirector(int id)
     {
-        var director =  _context.Director.
-            Where(d => d.directorID == id).
-            FirstOrDefault();
-
-        if (director is Director)
+        var director = _context.Director.FirstOrDefault(a => a.directorID == id);
+        if (director != null)
         {
             return Ok(director);
         }
-        
         return NotFound();
     }
 
@@ -50,49 +82,25 @@ public class DirectorController : BaseController
         return View(director);
     }
 
-
-
     [HttpPost]
-    public async Task<IActionResult> PostDirector(Director director)
+    public async Task<ActionResult<Director>> PostDirector(Director director)
     {
-        
         _context.Director.Add(director);
         await _context.SaveChangesAsync();
         return Ok(director);
-        
     }
 
-    [HttpPut]
-    public async Task<IActionResult> PutDirector(Director director)
+    [HttpPost("create-Director")]
+    public async Task<IActionResult> CreateDirector([FromForm] string nome, [FromForm] int idade, [FromForm] string bio)
     {
-        var dir = _context.Director. 
-            Where(d => d.directorID == director.directorID). 
-            FirstOrDefault();
-
-        if (dir is not Director)
+        var director = new Director
         {
-            return NotFound();
-        }
-        dir.nome = director.nome;
-        
+            nome = nome,
+            idade = idade,
+            bio = bio
+        };
+        _context.Director.Add(director);
         await _context.SaveChangesAsync();
-        return Ok(director);
-    }
-
-    [HttpDelete]
-    public async Task<IActionResult> DeleteDirector([FromBody] int id)
-    {
-        var dir = _context.Director. 
-            Where(d => d.directorID == id). 
-            FirstOrDefault();
-
-        if (dir == null)
-        {
-            return NotFound();
-        }
-        
-        _context.Director.Remove(dir);
-        await _context.SaveChangesAsync();
-        return Ok("Director removido com sucesso!");
+        return RedirectToAction("Directors");
     }
 }
