1901fdf64cc5bea56a401eada69a79a2bcfd6832
GCravo04 <gabicravo04@gmail.com>
2025-07-10 18:59:30 +0100

    Página de Admin para Cruds para Actors

--- dweb/Controllers/ActorController.cs
+++ dweb/Controllers/ActorController.cs
@@ -2,97 +2,111 @@
 using dweb.Models;
 using Microsoft.AspNetCore.Mvc;
 
-namespace dweb.Controllers;
-
-[Route("api/[controller]")]
-[ApiController]
-
-public class ActorController : BaseController
+namespace dweb.Controllers
 {
-    
-    private readonly AppDbContext _context;
-
-    public ActorController(AppDbContext context) : base(context)
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ActorController : BaseController
     {
-        _context = context;
-    }
+        private readonly AppDbContext _context;
 
-    [HttpGet]
-    public async Task<ActionResult<IEnumerable<Actor>>> GetActors()
-    {
-        var actors =  _context.Actor.ToList();
-        return actors;
-    }
-
-    [HttpGet("{id}")]
-    public async Task<ActionResult<Actor>> GetActor(int id)
-    {
-        var actor = _context.Actor.Where(a => a.actorID == id).
-            FirstOrDefault();
-
-        if (actor is Actor)
+        public ActorController(AppDbContext context) : base(context)
         {
+            _context = context;
+        }
+
+        [HttpGet]
+        [Route("/Actor/Actors")]
+        public IActionResult Actors()
+        {
+            var actors = _context.Actor.ToList();
+            return View("actors", actors);
+        }
+
+        [HttpPost("update-actor")]
+        public async Task<IActionResult> UpdateActor([FromForm] int actorID, [FromForm] string nome, [FromForm] int idade, [FromForm] string bio)
+        {
+            var a = _context.Actor.FirstOrDefault(x => x.actorID == actorID);
+            if (a == null)
+            {
+                return NotFound();
+            }
+            a.nome = nome;
+            a.idade = idade;
+            a.bio = bio;
+            await _context.SaveChangesAsync();
+            return RedirectToAction("ActorDetails", new { id = actorID });
+        }
+
+        
+        [HttpPost("delete-actor")]
+        public async Task<IActionResult> DeleteActor([FromForm] int actorID)
+        {
+            var actor = _context.Actor.FirstOrDefault(a => a.actorID == actorID);
+            if (actor == null)
+            {
+                return NotFound();
+            }
+            _context.Actor.Remove(actor);
+            await _context.SaveChangesAsync();
+            return RedirectToAction("Actors");
+        }
+
+        
+        [HttpGet("all")]
+        public ActionResult<IEnumerable<Actor>> GetActors()
+        {
+            var actors = _context.Actor.ToList();
+            return actors;
+        }
+
+       
+        [HttpGet("{id}")]
+        public ActionResult<Actor> GetActor(int id)
+        {
+            var actor = _context.Actor.FirstOrDefault(a => a.actorID == id);
+            if (actor != null)
+            {
+                return Ok(actor);
+            }
+            return NotFound();
+        }
+
+      
+        [HttpGet]
+        [Route("/Actor/ActorDetails/{id}")]
+        public IActionResult ActorDetails(int id)
+        {
+            var actor = _context.Actor.FirstOrDefault(a => a.actorID == id);
+            if (actor == null)
+            {
+                return NotFound();
+            }
+            return View(actor);
+        }
+
+        
+        [HttpPost]
+        public async Task<ActionResult<Actor>> PostActor(Actor actor)
+        {
+            _context.Actor.Add(actor);
+            await _context.SaveChangesAsync();
             return Ok(actor);
         }
-        return NotFound();
-    }
 
-    [HttpGet]
-    [Route("/Actor/ActorDetails/{id}")]
-    public IActionResult ActorDetails(int id)
-    {
-        var actor = _context.Actor.FirstOrDefault(a => a.actorID == id);
-        if (actor == null)
+      
+        [HttpPost("create-Actor")]
+        public async Task<IActionResult> CreateActor([FromForm] string nome, [FromForm] int idade, [FromForm] string bio)
         {
-            return NotFound();
+            var actor = new Actor
+            {
+                nome = nome,
+                idade = idade,
+                bio = bio
+            };
+            _context.Actor.Add(actor);
+            await _context.SaveChangesAsync();
+            return RedirectToAction("Actors");
         }
-        return View(actor);
-    }
-    
-    [HttpPost]
-    public async Task<ActionResult<Actor>> PostActor(Actor actor)
-    {
-        
-        _context.Actor.Add(actor);
-        await _context.SaveChangesAsync();
-        return Ok(actor);
-    }
-
-    
-    [HttpPut("{id}")]
-    public async Task<ActionResult<Actor>> PutActor(Actor actor)
-    {
-        var a =  _context.Actor.
-                Where(a => a.actorID == actor.actorID).
-                FirstOrDefault();
-        
-        if (a == null)
-        {
-            return NotFound();
-        }
-        
-        a.nome = actor.nome;
-        a.idade = actor.idade;
-        a.bio = actor.bio;
-        
-        //_context.Actor.Update(actor);
-        await _context.SaveChangesAsync();
-        return Ok(actor);
-    }
-
-    [HttpDelete]
-    public async Task<ActionResult<Actor>> DeleteActor([FromBody] int id)
-    {
-        var act = _context.Actor.Where(a => a.actorID == id). 
-                FirstOrDefault();
-
-        if (act == null)
-        {
-            return NotFound();
-        }
-        
-        _context.Actor.Remove(act);
-        await _context.SaveChangesAsync();
-        return Ok("Actor removido com sucesso!");
     }
 }
